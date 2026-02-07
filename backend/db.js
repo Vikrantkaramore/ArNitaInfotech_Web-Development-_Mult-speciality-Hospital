aff06682db4c928a8e22526bf720ff1172002c5d
@Vikrantkaramore <vikrantkaramore@gmail.com>
2026-02-07 23:27:55 +0530

    Refactor database connection setup by removing unnecessary configuration options

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,12 @@
 const mysql = require("mysql2");
 
-const dbConfig = {
+const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-};
-
-const db = mysql.createPool(dbConfig);
+});
 
 db.getConnection((err, connection) => {
   if (err) {
