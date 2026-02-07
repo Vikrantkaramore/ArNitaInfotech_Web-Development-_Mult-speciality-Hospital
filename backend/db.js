bbe51546bb4c93c59467f7061a03ae000006e9d9
@Vikrantkaramore <vikrantkaramore@gmail.com>
2026-02-07 23:24:36 +0530

    remove fallback values

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const dbConfig = {
-  host: process.env.DB_HOST || process.env.MYSQLHOST || "localhost",
-  user: process.env.DB_USER || process.env.MYSQLUSER || "root",
-  password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD || "Pass@2026",
-  database: process.env.DB_NAME || process.env.MYSQLDATABASE || "hospital_db",
-  port: process.env.DB_PORT || process.env.MYSQLPORT || 3306,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
